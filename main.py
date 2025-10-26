19b3707eb191933c4a77b5c4eed9426c80ed6cc7
Oksana Mazurak <122182015+oksanamazurak@users.noreply.github.com>
2025-10-26 18:03:25 +0200

    Add dockerfile and docker-compose (#2)
    Co-authored-by: Oksana <oksana.mazurak@globaldev.tech>

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from pyspark.sql import SparkSession
 from src.data_extraction import load_taxi_data
 
 if __name__ == "__main__":
-    file_path = "data/yellow_tripdata_2015-01.csv"
+    spark = SparkSession.builder \
+        .appName("NYC Taxi Data Extraction") \
+        .master("spark://spark-master:7077") \
+        .getOrCreate()
 
-    df = load_taxi_data(file_path)
+    spark.sparkContext.setLogLevel("INFO")
+
+    file_path = "/app/data/yellow_tripdata_2015-01.csv"
+    df = load_taxi_data(spark, file_path)
 
     print(f"Кількість рядків у DataFrame: {df.count()}")
     print(f"Кількість колонок: {len(df.columns)}")
+
+    spark.stop()
